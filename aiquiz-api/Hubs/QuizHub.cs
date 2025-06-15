fa84a5dc220d9e6bb60c1a9d7a49edc5c8a7e6fc
kliang <kenliangnz@mail.com>
2025-06-15 13:58:07 +1200

    Add SingleR Hub to api project

---
+++ aiquiz-api/Hubs/QuizHub.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.SignalR;
+
+namespace aiquiz_api.Hubs
+{
+    public class QuizHub : Hub
+    {
+        // You can add methods here to send/receive messages
+        // Example: public async Task SendMessage(string user, string message) { ... }
+    }
+}
