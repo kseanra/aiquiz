01bc98cd96d70620fceffa1dcd9d2b4986f247dd
kliang <kenliangnz@mail.com>
2025-07-16 21:22:17 +1200

    handle connection retry

--- quiz-client/Program.cs
+++ quiz-client/Program.cs
@@ -23,12 +23,24 @@ static async Task RunTaskAsync(string? value)
     {
         var connection = new HubConnectionBuilder()
             .WithUrl($"http://localhost:5000/quizhub", Microsoft.AspNetCore.Http.Connections.HttpTransportType.WebSockets | Microsoft.AspNetCore.Http.Connections.HttpTransportType.LongPolling) // Optionally add a query param for identification
+            .WithAutomaticReconnect(new[]
+            {
+                TimeSpan.FromSeconds(0),
+                TimeSpan.FromSeconds(2),
+                TimeSpan.FromSeconds(10),
+                TimeSpan.FromSeconds(30)
+            }   ) // Retry intervals
             .ConfigureLogging(logging =>
             {
                 logging.SetMinimumLevel(LogLevel.Debug);
             })
             .Build();
 
+        connection.Closed += async (error) =>
+        {
+            //Console.WriteLine($"Connection closed: {error?.Message}");
+            await Task.Delay(500);
+        };
         connection.On("RequestSetTopic", () =>
         {
             _ = Task.Run(async () =>
@@ -38,7 +50,7 @@ static async Task RunTaskAsync(string? value)
                 await connection.InvokeAsync("SetQuizTopic", "NBA", 4);
             });
         });
-
+    
         connection.On<Quiz>("ReceiveQuestion", (question) =>
         {
             _ = Task.Run(async () =>
@@ -62,15 +74,21 @@ static async Task RunTaskAsync(string? value)
             // User status display removed
         });
 
+
+
         connectionTasks.Add(
             Task.Run(async () =>
             {
-                await Task.Delay(new Random().Next(1000, 2000));
-                await connection.StartAsync();
-                await Task.Delay(500);
-                await connection.InvokeAsync("SubmitName", $"Bot_{Guid.NewGuid()}");
-                await Task.Delay(500);
-                await connection.InvokeAsync("ReadyForGame", true);
+                try {
+                    await Task.Delay(new Random().Next(1000, 2000));
+                    await connection.StartAsync();
+                    await Task.Delay(500);
+                    await connection.InvokeAsync("SubmitName", $"Bot_{Guid.NewGuid()}");
+                    await Task.Delay(500);
+                    await connection.InvokeAsync("ReadyForGame", true);
+                } catch (Exception ex) {
+                    Console.WriteLine($"Error in connection: {ex.Message}");
+                }
             }));
     }
 
